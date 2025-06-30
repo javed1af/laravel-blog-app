70cda58d0818a99d883e1e530881297e1f9edd7e
javed1af <javed1af@gmail.com>
2025-06-30 14:40:54 +0430

    fix small issues

--- resources/views/notifications/create.blade.php
+++ resources/views/notifications/create.blade.php
@@ -1,11 +1,9 @@
 <x-app-layout>
-    @if($user->is_admin)
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Create Notification') }}
         </h2>
     </x-slot>
-    @end
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
